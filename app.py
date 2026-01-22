405879a314f340c61a5c9a72aa9751fa580b40b9
Olaf Bulas <olaf.bulas@gmail.com>
2026-01-22 17:59:49 +0100

    Removed Analytics From Stage 1
    The analytics part is ony needed in stage 2

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ lat, lon = ui.render_sidebar()
 
 # --- ROUTER ---
 if st.session_state.step == 1:
-    stage_1_preview.show(lat, lon, get_model)
+    stage_1_preview.show()
 
 elif st.session_state.step == 2:
     stage_2_refine.show(get_model)
